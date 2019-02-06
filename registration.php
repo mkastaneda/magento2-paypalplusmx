4b96e029096628cf2c3628d70458756c9e4d61e0
yagator <yagator@gmail.com>
2019-02-06 17:39:40 -0600

    module pattern name chenge

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'qbo_PayPalPlusMx',
+    'Qbo_PayPalPlusMx',
     __DIR__
 );
